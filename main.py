6befe6ba99ce600a7f634270b99af9b55a077915
Brexy-Services <jacobstephens1241255@gmail.com>
2024-07-07 21:52:02 +0100

    Create main.py
    Main py .File

---
+++ main.py
@@ -0,0 +1,174 @@
+import  random,     threading, requests, discord, os
+
+from    discord.ext import commands
+
+color        = 0x71368A
+token 	     = "PLACE YOUR TOKEN HERE :)"
+bot_channel  = 1258184985857495132
+
+intents     = discord.Intents.all()
+bot         = commands.Bot(command_prefix=".", help_command=None, intents=intents)
+threads     = 25 # Lower this if your bot does not respond while sending followers
+
+def GetUser(token):
+
+    headers = {
+    'accept': '*/*',
+    'accept-language': 'en-US,en;q=0.9',
+    'authorization': 'Bearer ' + token,
+    'cache-control': 'no-cache',
+    'origin': 'https://barrycarlyon.github.io',
+    'pragma': 'no-cache',
+    'priority': 'u=1, i',
+    'referer': 'https://barrycarlyon.github.io/',
+    'sec-ch-ua': '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'cross-site',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
+    }
+
+    response = requests.get('https://id.twitch.tv/oauth2/validate', headers=headers)
+    try:    
+        return response.json()['user_id']
+    except:
+        return None
+
+
+def get_id(user):
+    json = {"operationName": "ChannelShell",
+            "variables": {
+                "login": user
+            },
+            "extensions": {
+                "persistedQuery": {
+                    "version": 1,
+                    "sha256Hash": "580ab410bcd0c1ad194224957ae2241e5d252b2c5173d8e0cce9d32d5bb14efe"
+                }
+            }
+        }
+
+    headers = {
+        'Client-ID': 'kimne78kx3ncx6brgo4mv6wki5h1ko'
+    }
+
+    r = requests.post('https://gql.twitch.tv/gql', json=json, headers=headers)
+    if 'data' in r.json() and 'userOrError' in r.json()['data']:
+        user_data = r.json()['data']['userOrError']
+        if 'id' in user_data:
+            return user_data['id']
+        
+    return None
+
+
+def GetFollowCount(channel):
+
+    headers = {
+        'Authorization': 'Oath xy16yngeyvtptbo2piyw018tqcikgn',
+        "Client-ID": "kd1unb4b3q4t58fwlpcbzcbnm76a8fp",
+        'Content-Type': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
+
+    query = '''
+    query {
+      user(login: "%s") {
+        followers {
+          totalCount
+        }
+      }
+    }
+    ''' % channel
+
+    url = 'https://gql.twitch.tv/gql'
+    payload = [{'operationName': None, 'variables': {}, 'extensions': {}, 'query': query}]
+
+    response = requests.post(url, headers=headers, json=payload)
+    try:
+        
+        return response.json()[0]['data']['user']['followers']['totalCount']
+    
+    except:
+        
+        return None
+
+
+def follow(target_id , amount):
+
+    for i in range(int(amount)):
+
+        tokens = random.choice(open("tokens.txt", "r" ).read().splitlines())
+
+        payload = {
+            "operationName": "FollowUserMutation",
+            "variables": {
+                "targetId": target_id,
+                "disableNotifications": False
+            },
+            "extensions": {
+                "persistedQuery": {
+                    "version": 1,
+                    "sha256Hash": "cd112d9483ede85fa0da514a5657141c24396efbc7bac0ea3623e839206573b8"
+                }
+            }
+        }
+
+        headers = {
+            "Api-Consumer-Type": "mobile; Android/1500000",
+            "Authorization": f"OAuth {tokens}",
+            "Client-ID": "kd1unb4b3q4t58fwlpcbzcbnm76a8fp",
+            "Connection": "Keep-Alive",
+            "Content-Type": "application/json",
+            "Host": "gql.twitch.tv",
+            "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 7.1.2; SM-G988N Build/NRD90M) tv.twitch.android.app/15.0.0/1500000",
+            "X-APOLLO-OPERATION-NAME": "FollowUserMutation",
+            "X-App-Version": "15.0.0",
+        }
+        proxy_list = open('proxys.txt','r').read().splitlines()
+
+        proxy = random.choice(proxy_list)
+        proxies = {
+            'http': f'http://{proxy}/',
+            'https':f'http://{proxy}/'
+        }
+        repsonse = requests.post('https://gql.twitch.tv/gql', json=payload, headers=headers, proxies=proxies)
+        print(repsonse.text)
+
+
+@bot.command()
+async def tfollow(ctx, channel):
+
+    if ctx.channel.id == int(bot_channel):
+
+        await ctx.message.delete()
+
+        target_id = get_id(channel)
+
+        if target_id is None:
+
+            embed = discord.Embed(title=f"Failed to find channel", description=f"Bot failed to find the inputed channel -> `{channel}`", color=color)
+            await ctx.send(embed=embed, delete_after=5)
+            return
+                
+        embed = discord.Embed(title="Follow Bot", description=f"Sending Followers to `{channel}`.", color=color)
+        embed.add_field(name="Follow Count", value=f"Currently has {GetFollowCount(channel)} followers.")
+        embed.add_field(name="Sending", value=f"Sending 50 followers.")
+        await ctx.send(embed=embed, delete_after=5)
+
+        for _ in range(threads):
+            threading.Thread(target=follow, args=[target_id, 50/threads]).start()
+    else:
+        await ctx.message.delete()
+    
+
+@bot.event
+async def on_ready():
+    os.system("cls")
+    print('Bot online')
+    with open("tokens.txt", 'r') as ttokens:
+        token_count = len(ttokens.readlines())
+        print(f"\nFollow Stock > {token_count}")
+
+bot.run(token)
